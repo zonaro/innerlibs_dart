689ec3ab5148bf38c72fbd73de08b6eb9add7bb1
Zonaro <zonaro@outlook.com>
2023-09-25 10:35:00 -0300

    -

--- lib/future_awaiter.dart
+++ lib/future_awaiter.dart
@@ -1,40 +1,35 @@
-library innerlibs;
-
 import 'package:flutter/material.dart';
 
 class FutureAwaiter<T> extends StatelessWidget {
   final Future<T> future;
   final Widget loadingWidget;
   final Widget Function(T data) doneWidget;
-  final Widget errorWidget;
-  final Widget? doneWithoutDataWidget;
+  final Widget doneWithoutDataWidget;
+  final Widget Function(Object error) errorWidget;
 
-  const FutureAwaiter({
-    super.key,
+  const FutureAwaiter({super.key, 
     required this.future,
     required this.loadingWidget,
     required this.doneWidget,
+    required this.doneWithoutDataWidget,
     required this.errorWidget,
-    this.doneWithoutDataWidget,
   });
 
   @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<T>(
+  Widget build(BuildContext context) => FutureBuilder<T>(
       future: future,
       builder: (BuildContext context, AsyncSnapshot<T> snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return loadingWidget;
         } else {
           if (snapshot.hasError) {
-            return errorWidget;
-          } else if (snapshot.hasData && snapshot.data != null) {
-            return doneWidget(snapshot.data as T);
+            return errorWidget(snapshot.error!);
+          } else if (snapshot.hasData) {
+            return doneWidget(snapshot.data!);
           } else {
-            return doneWithoutDataWidget ?? errorWidget;
+            return doneWithoutDataWidget;
           }
         }
       },
     );
-  }
 }
