c446281f40aa58b5e4f502419d168f5842e6d2e9
Zonaro <zonaro@outlook.com>
2023-09-28 04:46:28 -0300

    -

--- lib/num_extensions.dart
+++ lib/num_extensions.dart
@@ -6,4 +6,5 @@ import 'package:string_extensions/string_extensions.dart';
 extension NumExtensions on num {
   
   String quantityText(String plural, [String singular = ""]) => (round() == 1 || round() == -1) ? singular.ifBlank(plural.singular)! : plural;
+  String quantityTextPt(String plural, [String singular = ""]) => (round() == 1 || round() == -1) ? singular.ifBlank(plural.singularPt)! : plural;
 }
