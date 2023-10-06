a5741000f1e58a347878e885c6cbd7f51d472628
Zonaro <zonaro@outlook.com>
2023-10-06 16:04:23 -0300

    -

--- lib/color_extensions.dart
+++ lib/color_extensions.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 
-import 'package:string_extensions/string_extensions.dart';
 
 extension ColorExt on Color {
   String get hexadecimal => '#${value.toRadixString(16).padLeft(8, '0')}';
