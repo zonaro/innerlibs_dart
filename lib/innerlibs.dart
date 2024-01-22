567009f38e7bd480f5dc423143ffb66f0d52cc98
Zonaro <zonaro@outlook.com>
2024-01-22 13:00:28 -0300

    -

--- lib/innerlibs.dart
+++ lib/innerlibs.dart
@@ -9,6 +9,7 @@ export 'string_extensions.dart';
 export 'restart_widget.dart';
 export 'future_awaiter.dart';
 export 'util.dart';
+
 export 'package:string_extensions/string_extensions.dart';
 export 'package:string_extensions/string_helpers.dart';
 export 'package:awesome_extensions/awesome_extensions.dart';
