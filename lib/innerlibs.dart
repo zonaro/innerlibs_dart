ab084a91e3b5127a415a04474011b6ef844d5bad
Zonaro <zonaro@outlook.com>
2024-01-24 19:34:47 -0300

    -

--- lib/innerlibs.dart
+++ lib/innerlibs.dart
@@ -3,6 +3,12 @@ library innerlibs;
 
 export 'brazil/brazil_tools.dart';
 
+export 'widgets/avatar_image.dart';
+export 'widgets/future_awaiter.dart';
+export 'widgets/nil.dart';
+export 'widgets/restart_widget.dart';
+export 'widgets/shimmer.dart';
+
 export 'color_extensions.dart';
 export 'general_extensions.dart';
 export 'list_extensions.dart';
@@ -11,9 +17,8 @@ export 'num_extensions.dart';
 export 'string_extensions.dart';
 export 'string_helpers.dart';
 export 'util.dart';
+ 
 
-export 'widgets/future_awaiter.dart';
-export 'widgets/avatar_image.dart';
 
 
  
