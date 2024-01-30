047a3a874197e1b2efddc370aeb6c861ad8f49cd
Zonaro <zonaro@outlook.com>
2024-01-30 18:47:30 -0300

    -

--- lib/view_extensions.dart
+++ lib/view_extensions.dart
@@ -12,21 +12,21 @@ extension ViewExtension on BuildContext {
   double get physicalWidth => physicalScreenSize.width;
   double get physicalHeight => physicalScreenSize.height;
 
-//Size in logical pixels
+  //Size in logical pixels
   Size get logicalScreenSize => flutterView.physicalSize / pixelRatio;
   double get logicalWidth => logicalScreenSize.width;
   double get logicalHeight => logicalScreenSize.height;
 
-//Padding in physical pixels
+  //Padding in physical pixels
   ViewPadding get padding => flutterView.padding;
 
-//Safe area paddings in logical pixels
+  //Safe area paddings in logical pixels
   double get paddingLeft => flutterView.padding.left / flutterView.devicePixelRatio;
   double get paddingRight => flutterView.padding.right / flutterView.devicePixelRatio;
   double get paddingTop => flutterView.padding.top / flutterView.devicePixelRatio;
   double get paddingBottom => flutterView.padding.bottom / flutterView.devicePixelRatio;
 
-//Safe area in logical pixels
+  //Safe area in logical pixels
   double get safeWidth => logicalWidth - paddingLeft - paddingRight;
   double get safeHeight => logicalHeight - paddingTop - paddingBottom;
 }
