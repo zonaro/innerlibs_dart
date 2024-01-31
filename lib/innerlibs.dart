9b96dcdc8c363c40275daafff2ae2961c73f55ca
Kaizonaro <zonaro@outlook.com>
2024-01-31 06:15:15 -0300

    -

--- lib/innerlibs.dart
+++ lib/innerlibs.dart
@@ -31,6 +31,7 @@ export 'text_style_extension.dart';
 export 'theme_extension.dart';
 export 'tooltip_extension.dart';
 export 'util.dart';
+export 'widget_ext.dart';
 export 'widgets/avatar_image.dart';
 export 'widgets/future_awaiter.dart';
 export 'widgets/nil.dart';
