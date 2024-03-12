d0c7866915e84b302627ae025dc229a9ce1abe83
Zonaro <zonaro@outlook.com>
2024-03-12 19:03:54 -0300

    -

--- lib/sql_row_extensions.dart
+++ lib/sql_row_extensions.dart
@@ -2,17 +2,25 @@ import 'package:innerlibs/innerlibs.dart';
 
 extension SqlRowExtensions on JsonRow {
   String asUpsertCommand(String tableName, [Map<String, dynamic>? primaryKeys, bool nullAsBlank = false]) {
-    var upsertMap = Map.from(this);
     if (primaryKeys.isValid) {
-      primaryKeys!.keys.forEach(upsertMap.remove);
-      String updates = upsertMap.entries.map((e) => "[${e.key}] = ${(e.value as Object?).asSqlValue(nullAsBlank)}").join(', ');
-      String whereClause = primaryKeys.entries.map((e) => "[${e.key}] = ${(e.value as Object?).asSqlValue(nullAsBlank)}").join(' AND ');
-      return 'UPDATE $tableName SET $updates WHERE $whereClause;';
+      return asUpdateCommand(tableName, primaryKeys!, nullAsBlank);
     } else {
-      String columns = upsertMap.keys.map((e) => "[$e]").join(', ');
-      String values = upsertMap.values.map((e) => (e as Object?).asSqlValue(nullAsBlank)).join(', ');
-      return 'INSERT INTO $tableName ($columns) VALUES ($values);';
+      return asInsertCommand(tableName, nullAsBlank);
     }
+  }
+
+  string asInsertCommand(string tableName, [bool nullAsBlank = false]) {
+    String columns = keys.map((e) => "[$e]").join(', ');
+    String values = this.values.map((e) => (e as Object?).asSqlValue(nullAsBlank)).join(', ');
+    return 'INSERT INTO $tableName ($columns) VALUES ($values);';
+  }
+
+  string asUpdateCommand(string tableName, JsonMap primaryKeys, [bool nullAsBlank = false]) {
+    var upsertMap = Map.from(this);
+    primaryKeys.keys.forEach(upsertMap.remove);
+    String updates = upsertMap.entries.map((e) => "[${e.key}] = ${(e.value as Object?).asSqlValue(nullAsBlank)}").join(', ');
+    String whereClause = primaryKeys.entries.map((e) => "[${e.key}] = ${(e.value as Object?).asSqlValue(nullAsBlank)}").join(' AND ');
+    return 'UPDATE $tableName SET $updates WHERE $whereClause;';
   }
 
   String asDeleteCommand(String tableName) {
