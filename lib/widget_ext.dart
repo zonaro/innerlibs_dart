ad21f0d74eb8ae8edbe1ce2c6638749bd3fc8fe1
Zonaro <zonaro@outlook.com>
2024-03-22 15:29:10 -0300

    -

--- lib/widget_ext.dart
+++ lib/widget_ext.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 extension WidgetExt on Widget {
   /// wrap a widget using a [Function] if [test] is true
-  Widget wrapIf(bool test, Widget Function(Widget children) func) {
+  Widget wrapIf(bool test, Widget Function(Widget wrappedChild) func) {
     if (test) {
       return func(this);
     } else {
