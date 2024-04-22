3e954cbe0cd2bbdf4814e20e4ac4cc29dbf29e74
Kaizonaro <zonaro@outlook.com>
2024-04-22 09:31:02 -0300

    -

--- lib/color_extensions.dart
+++ lib/color_extensions.dart
@@ -1,8 +1,7 @@
 import 'dart:ui';
-
+import 'package:innerlibs/innerlibs.dart';
 
 extension ColorExtensions on Color {
-  
-  String get hexadecimal => '#${value.toRadixString(16).padLeft(8, '0')}';
-
+  String get hexadecimal => '#${value.toRadixString(16).removeFirst(2).padLeft(6, '0')}';
+  String get alphaHexadecimal => '#${value.toRadixString(16).padLeft(8, '0')}';
 }
