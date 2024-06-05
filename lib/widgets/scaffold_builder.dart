8dfefc85dc15b75c502c8b27297391420413bcab
Zonaro <zonaro@outlook.com>
2024-06-05 09:43:00 -0300

    -

--- lib/widgets/scaffold_builder.dart
+++ lib/widgets/scaffold_builder.dart
@@ -1,59 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:innerlibs/innerlibs.dart';
 
-class ScaffoldParts {
+class ScaffoldParts extends StatefulWidget {
   final List<MenuEntry> items;
 
-  final ValueNotifier<int> _currentIndex = ValueNotifier(0);
-
   final void Function(int oldIndex, int newIndex)? onIndexChange;
+  final ValueNotifier<int> currentIndex;
 
-  ScaffoldParts({
+  final Widget Function(BuildContext context, int currentIndex, MenuEntry entry, void Function(int value) onNavigationTap, List<BottomNavigationBarItem> bottomNavigationItems) builder;
+
+  const ScaffoldParts({
+    super.key,
     required this.items,
-    int currentIndex = 0,
     this.onIndexChange,
-  }) {
-    _currentIndex.value = currentIndex;
-  }
+    required this.builder,
+    required this.currentIndex,
+  });
 
-  Widget get page => items[_currentIndex.value].page;
-  int get currentIndex => _currentIndex.value;
-  string get title => items[_currentIndex.value].title;
-  IconData get icon => items[_currentIndex.value].icon;
-  IconData get actionIcon => items[_currentIndex.value].actionIcon ?? activeIcon;
-  IconData get activeIcon => items[_currentIndex.value].activeIcon ?? icon;
-  string get actionTitle => items[_currentIndex.value].actionTitle | title;
+  @override
+  State<ScaffoldParts> createState() => _ScaffoldPartsState();
+}
+
+class _ScaffoldPartsState extends State<ScaffoldParts> {
+  
+
+  Widget get page => widget.items[widget.currentIndex.value].page;
+
+  int get currentIndex => widget.currentIndex.value;
+
+  string get title => widget.items[widget.currentIndex.value].title;
+
+  IconData get icon => widget.items[widget.currentIndex.value].icon;
+
+  IconData get actionIcon => widget.items[widget.currentIndex.value].actionIcon ?? activeIcon;
+
+  IconData get activeIcon => widget.items[widget.currentIndex.value].activeIcon ?? icon;
+
+  string get actionTitle => widget.items[widget.currentIndex.value].actionTitle | title;
 
   void Function(int) get onNavigationTap {
     return (int value) {
-      if (_currentIndex.value == value) {
-        var funcs = items[_currentIndex.value].action;
+      if (widget.currentIndex.value == value) {
+        var funcs = widget.items[widget.currentIndex.value].action;
         if (funcs != null) {
           (funcs)();
         }
       } else {
-        int old = _currentIndex.value;
-        _currentIndex.value = value;
-        if (onIndexChange != null) {
-          (onIndexChange)!(old, value);
+        int old = widget.currentIndex.value;
+        widget.currentIndex.value = value;
+        if (widget.onIndexChange != null) {
+          (widget.onIndexChange)!(old, value);
         }
       }
     };
   }
 
-  Widget? get floatingActionButton => items[_currentIndex.value].floatingActionButton;
-  FloatingActionButtonLocation? get floatingActionButtonLocation => items[_currentIndex.value].floatingActionButtonLocation;
+  Widget? get floatingActionButton => widget.items[widget.currentIndex.value].floatingActionButton;
+
+  FloatingActionButtonLocation? get floatingActionButtonLocation => widget.items[widget.currentIndex.value].floatingActionButtonLocation;
 
   List<BottomNavigationBarItem> get bottomNavigationBarItems => [
-        for (var entry in items)
+        for (var entry in widget.items)
           BottomNavigationBarItem(
             icon: Icon(entry.icon),
             activeIcon: Icon((entry.action == null ? null : entry.actionIcon) ?? entry.activeIcon ?? entry.icon),
-            label: _currentIndex.value == items.indexOf(entry) ? (entry.action == null ? null : entry.actionTitle) | entry.title : entry.title,
-            tooltip: _currentIndex.value == items.indexOf(entry) ? (entry.action == null ? null : entry.actionTooltip) | entry.tooltip : entry.tooltip,
+            label: widget.currentIndex.value == widget.items.indexOf(entry) ? (entry.action == null ? null : entry.actionTitle) | entry.title : entry.title,
+            tooltip: widget.currentIndex.value == widget.items.indexOf(entry) ? (entry.action == null ? null : entry.actionTooltip) | entry.tooltip : entry.tooltip,
             backgroundColor: entry.backgroundColor,
           ),
       ];
+
+  @override
+  Widget build(BuildContext context) => Builder(builder: (context) => widget.builder(context, currentIndex, widget.items[currentIndex], onNavigationTap, bottomNavigationBarItems));
 }
 
 typedef MenuEntries = List<MenuEntry>;
