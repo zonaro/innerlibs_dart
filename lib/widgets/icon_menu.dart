11e3a13ae22c56e00554bc1540b4d983e2f932c0
Zonaro <zonaro@outlook.com>
2024-06-28 13:30:59 -0300

    refactor: Atualizar IconMenu para suportar cor e largura máxima
    Essa alteração atualiza a classe IconMenu para adicionar suporte à personalização da cor e largura máxima do menu. Agora, é possível definir uma cor personalizada para os ícones do menu e especificar uma largura máxima para o menu. Isso melhora a flexibilidade e usabilidade do componente IconMenu.

--- lib/widgets/icon_menu.dart
+++ lib/widgets/icon_menu.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:innerlibs/innerlibs.dart';
 
 class IconMenu extends StatefulWidget {
-  final List<IconMenuButton> buttons;
-
-  const IconMenu({Key? key, required this.buttons}) : super(key: key);
+  final List<IconMenuItem> buttons;
+  final double? maxWidth;
+  final Color? color;
+  const IconMenu({Key? key, required this.buttons, this.maxWidth, this.color}) : super(key: key);
 
   @override
   createState() => _IconMenuState();
@@ -12,53 +14,118 @@ class IconMenu extends StatefulWidget {
 class _IconMenuState extends State<IconMenu> {
   @override
   Widget build(BuildContext context) {
+    var iconSize = context.theme.iconTheme.size ?? 24.0;
+
+    var adaptedSize = 40.0 * (iconSize / 24.0);
+
+    Color? color = widget.color ?? context.theme.iconTheme.color;
+
+    var fullsize = widget.buttons.length * (adaptedSize);
     return LayoutBuilder(
       builder: (context, constraints) {
+        var maxWidth = widget.maxWidth ?? constraints.maxWidth;
+
         if (widget.buttons.isEmpty) {
           return const SizedBox.shrink();
         }
         if (widget.buttons.length == 1) {
-          return IconButton(
-            tooltip: widget.buttons.first.text,
-            icon: widget.buttons.first.icon,
-            onPressed: widget.buttons.first.onPressed,
-          );
+          var button = widget.buttons.first;
+          if (button is IconMenuButton) {
+            if (button.showText) {
+              return TextButton.icon(
+                icon: Icon(
+                  button.icon,
+                  color: button.color ?? color,
+                  size: iconSize,
+                ),
+                label: Text(button.text),
+                onPressed: button.onPressed,
+              );
+            } else {
+              return IconButton(
+                tooltip: button.text,
+                icon: Icon(
+                  button.icon,
+                  color: button.color ?? color,
+                  size: iconSize,
+                ),
+                onPressed: button.onPressed,
+              );
+            }
+          } else if (button is IconMenuDivider) {
+            return const SizedBox.shrink();
+          } else {
+            throw Exception("Invalid IconMenuItem type");
+          }
         }
 
-        if (constraints.maxWidth < 600) {
-          return PopupMenuButton<IconMenuButton>(
-            icon: const Icon(Icons.menu),
+        consoleLog("fullsize: $fullsize, width: $maxWidth");
+        if (fullsize > maxWidth) {
+          return PopupMenuButton<IconMenuItem>(
+            icon: Icon(
+              Icons.menu,
+              size: iconSize,
+              color: color,
+            ),
             itemBuilder: (context) {
-              return widget.buttons.map((button) {
-                return PopupMenuItem<IconMenuButton>(
-                  value: button,
-                  onTap: button.onPressed,
-                  child: ListTile(
-                    leading: button.icon,
-                    title: Text(button.text),
-                  ),
-                );
-              }).toList();
+              return [
+                for (var button in widget.buttons)
+                  if (button is IconMenuDivider)
+                    const PopupMenuDivider()
+                  else if (button is IconMenuButton)
+                    PopupMenuItem<IconMenuButton>(
+                      value: button,
+                      onTap: button.onPressed,
+                      child: ListTile(
+                        leading: Icon(
+                          button.icon,
+                          color: button.color ?? color,
+                          size: iconSize,
+                        ),
+                        title: Text(button.text).textColor(button.color ?? color),
+                      ),
+                    )
+                  else
+                    throw Exception("Invalid IconMenuItem type")
+              ];
             },
           );
         } else {
           return Row(
-            children: [
-              Expanded(
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: widget.buttons.map((button) {
-                      return IconButton(
-                        tooltip: button.text,
-                        icon: button.icon,
-                        onPressed: button.onPressed,
-                      );
-                    }).toList(),
-                  ),
-                ),
-              ),
-            ],
+            children: widget.buttons.map((button) {
+              if (button is IconMenuDivider) {
+                return Container(
+                  margin: const EdgeInsets.all(10),
+                  height: adaptedSize,
+                  width: 1,
+                  color: button.color ?? color,
+                );
+              } else if (button is IconMenuButton) {
+                if (button.showText) {
+                  return TextButton.icon(
+                    icon: Icon(
+                      button.icon,
+                      color: button.color ?? color,
+                      size: iconSize,
+                    ),
+                    label: Text(button.text).textColor(button.color ?? color),
+                    onPressed: button.onPressed,
+                  );
+                } else {
+                  return IconButton(
+                    tooltip: button.text,
+                    icon: Icon(
+                      button.icon,
+                      color: button.color ?? color,
+                      size: iconSize,
+                    ),
+                    onPressed: button.onPressed,
+                  );
+                }
+              } else {
+                throw Exception("Invalid IconMenuItem type");
+              }
+            }).toList(),
           );
         }
       },
@@ -66,14 +133,28 @@ class _IconMenuState extends State<IconMenu> {
   }
 }
 
-class IconMenuButton {
-  final Icon icon;
+abstract class IconMenuItem {
+  final Color? color;
+
+  IconMenuItem({this.color});
+}
+
+class IconMenuDivider extends IconMenuItem {
+  @override
+  IconMenuDivider({super.color});
+}
+
+class IconMenuButton extends IconMenuItem {
+  final IconData icon;
   final String text;
+  final bool showText;
   final void Function()? onPressed;
 
   IconMenuButton({
     required this.icon,
     required this.text,
+    this.showText = false,
     this.onPressed,
+    super.color,
   });
 }
