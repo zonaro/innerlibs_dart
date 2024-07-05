4e34c7efa1a3416984780e7510e8d1769b2172fb
Zonaro <zonaro@outlook.com>
2024-07-05 10:15:52 -0300

    feat: Adicionar widget FloatingArea

--- lib/widgets/floating_overlay.dart
+++ lib/widgets/floating_overlay.dart
@@ -1,34 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:innerlibs/innerlibs.dart';
 
-class FloatingOverlayWidget extends StatefulWidget {
+/// A widget that displays a floating overlay with a list of children widgets.
+///
+/// The [FloatingOverlay] widget is used to display a list of children widgets in an overlay.
+/// The children can be either a [DraggableChildren] or a regular [Widget].
+/// If the child is not of type [DraggableChildren], it will be automatically converted to a [Widget] using the [Object.forceWidget()] method.
+/// If the child is a [Widget], it will be wrapped into a [DraggableChildren] for consistency.
+///
+/// Example usage:
+/// ```dart
+/// FloatingOverlay(
+///   children: [
+///     DraggableChildren(
+///       child: Container(
+///         width: 100,
+///         height: 100,
+///         color: Colors.red,
+///       ),
+///     ),
+///     Text('Hello, World!'),
+///   ],
+/// )
+/// ```
+class FloatingArea extends StatefulWidget {
+  /// The background widget.
   final Widget? background;
-  final List<DraggableChildren> children;
 
-  const FloatingOverlayWidget({Key? key, required this.children, this.background}) : super(key: key);
+  /// The foreground widget.
+  final Widget? foreground;
+
+  /// The list of children widgets.
+  final List<dynamic> children;
+
+  /// A callback function that is called when the draggable widget is released.
+  final void Function(DraggableDetails, DraggableChildren)? onDragEnd;
+
+  /// A callback function that is called when the draggable widget is being dragged.
+  final void Function(DragUpdateDetails, DraggableChildren)? onDragUpdate;
+
+  /// Creates a [FloatingArea] widget.
+  ///
+  /// The [children] parameter is a list of draggable children.
+  /// The [background] parameter is an optional widget that appears behind the draggable children.
+  /// The [onDragEnd] parameter is a callback function that is called when a draggable child is released.
+  /// The [onDragUpdate] parameter is a callback function that is called when a draggable child is being dragged.
+  /// The [foreground] parameter is an optional widget that appears in front of the draggable children.
+  const FloatingArea({
+    Key? key,
+    required this.children,
+    this.background,
+    this.onDragEnd,
+    this.onDragUpdate,
+    this.foreground,
+  }) : super(key: key);
 
   @override
-  createState() => _FloatingOverlayWidgetState();
+  createState() => _FloatingAreaState();
 }
 
-class _FloatingOverlayWidgetState extends State<FloatingOverlayWidget> {
-  List<DraggableChildren> children = [];
+class _FloatingAreaState extends State<FloatingArea> {
+  final List<DraggableChildren> _children = [];
 
   @override
   void initState() {
     super.initState();
-    children.addAll(widget.children);
+    for (var w in widget.children) {
+      if (w == null) {
+        continue;
+      }
+      if (w is DraggableChildren) {
+        _children.add(w);
+      } else {
+        _children.add(DraggableChildren(x: 0, y: 0, child: (w as Object?).forceWidget()!));
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.indigo,
-      width: 400,
-      height: 400,
-      child: Stack(
+    return LayoutBuilder(
+      builder: (context, c) => Stack(
         children: [
           if (widget.background != null) widget.background!,
-          for (var w in widget.children)
+          for (var w in _children)
             Positioned(
               left: w.x,
               top: w.y,
@@ -38,29 +93,72 @@ class _FloatingOverlayWidgetState extends State<FloatingOverlayWidget> {
                   child: w.child,
                 ),
                 childWhenDragging: Container(),
+                onDragUpdate: (details) {
+                  setState(() {
+                    if (widget.onDragUpdate != null) widget.onDragUpdate!(details, w);
+                  });
+                },
                 onDragEnd: (details) {
                   setState(() {
-                    // Set the new position of the dragged widget
-                    final newPosition = details.offset;
+                    // Calculates local coordinates.
+                    // Local coordinates are relative, not absolute.
+                    var renderBox = context.findRenderObject() as RenderBox;
+                    final offsetLocalRelative = renderBox.globalToLocal(details.offset);
+
+                    double x = offsetLocalRelative.dx;
+                    double y = offsetLocalRelative.dy;
+
+                    if (offsetLocalRelative.dx > c.maxWidth) {
+                      x = c.maxWidth - 100;
+                    }
+                    if (offsetLocalRelative.dy > c.maxHeight) {
+                      y = c.maxHeight - 100;
+                    }
+
                     // Update the position of the widget in the children list
-                    var t = children[widget.children.indexOf(w)];
-                    t.x = newPosition.dx;
-                    t.y = newPosition.dy;
+                    var t = _children[widget.children.indexOf(w)];
+                    t.x = x;
+                    t.y = y;
+
+                    // Call the onDragEnd callback
+                    if (widget.onDragEnd != null) {
+                      widget.onDragEnd!(DraggableDetails(velocity: details.velocity, offset: Offset(x, y)), w);
+                    }
                   });
                 },
                 child: w.child,
               ),
             ),
+          if (widget.foreground != null) widget.foreground!,
         ],
       ),
     );
   }
 }
 
+/// A draggable child widget for [FloatingArea].
 class DraggableChildren {
-  final Widget child;
+  /// The child widget to be displayed in the [FloatingArea].
+  Widget child;
+
+  /// The x-coordinate of the child.
   double x;
+
+  /// The y-coordinate of the child.
   double y;
 
-  DraggableChildren({required this.child, required this.x, required this.y});
+  /// Additional data associated with this widget.
+  JsonMap data;
+
+  /// Creates a [DraggableChildren] widget.
+  ///
+  /// The [child] parameter is the widget that can be dragged.
+  /// The [x] parameter is the initial x-coordinate of the draggable child.
+  /// The [y] parameter is the initial y-coordinate of the draggable child.
+  DraggableChildren({
+    required this.child,
+    required this.x,
+    required this.y,
+    this.data = const {},
+  });
 }
