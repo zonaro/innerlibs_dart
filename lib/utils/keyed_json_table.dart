96bab53f1917b5fd2ac72e57a108385d7b7ee786
Zonaro <zonaro@outlook.com>
2024-07-25 17:27:05 -0300

    refactor: Update `KeyedJsonTable` to use single function parameter for getPair and getPairs methods

--- lib/utils/keyed_json_table.dart
+++ lib/utils/keyed_json_table.dart
@@ -40,25 +40,24 @@ import 'package:innerlibs/innerlibs.dart';
 
 class KeyedJsonTable<T extends Comparable> extends Iterable<JsonRow> {
   final string keyName;
-  late JsonTable table;
+  late JsonTable _table;
 
   /// Constructs a [KeyedJsonTable] with the specified [keyName] and [items].
   KeyedJsonTable({
     required this.keyName,
     JsonTable items = const [],
   }) {
-    table = items.toList(growable: true);
+    _table = [];
+    if (items.isNotEmpty) {
+      addAll(items);
+    }
   }
 
   /// Returns the [JsonRow] with the specified [key], or `null` if not found.
-  JsonRow? operator [](T key) => table.where((row) => ((row[keyName] as Object?).parseTo<T>()) == key).singleOrNull;
+  JsonRow? operator [](T key) => _table.where((row) => parseTo(row[keyName]) == key).singleOrNull;
 
   /// Sets the [JsonRow] with the specified [key] to the given [values].
-  void operator []=(T key, JsonRow values) {
-    var m = this[key];
-    m = m ?? values;
-    m.addAll(values);
-  }
+  void operator []=(T key, JsonRow values) => add(values..[keyName] = key);
 
   /// Returns a pair of values computed from the given [key] using the provided [func] and [valueFunc].
   /// The [func] is a function that takes a [JsonRow] and returns a value of type [T1].
@@ -73,15 +72,15 @@ class KeyedJsonTable<T extends Comparable> extends Iterable<JsonRow> {
   /// Returns a list of pairs computed from the elements in the table using the provided [keyFunc] and [valueFunc].
   /// The [keyFunc] is a function that takes a [JsonRow] and returns a value of type [T1].
   /// The [valueFunc] is a function that takes a [JsonRow] and returns a value of type [T2].
-  List<(T1, T2)> getPairs<T1, T2>((T1, T2) Function(JsonRow) func) => this.table.where((e) => e[keyName] != null).map((e) => getPair(e[keyName] as T, func)).toList();
+  List<(T1, T2)> getPairs<T1, T2>((T1, T2) Function(JsonRow) func) => this._table.where((e) => e[keyName] != null).map((e) => getPair(e[keyName] as T, func)).toList();
 
   /// Returns a map computed from the elements in the table using the provided [keyFunc] and [valueFunc].
   /// The [keyFunc] is a function that takes a [JsonRow] and returns a value of type [T1].
   /// The [valueFunc] is a function that takes a [JsonRow] and returns a value of type [T2].
-  Map<T1, T2> getMap<T1, T2>(T1 Function(JsonRow) keyFunc, T2 Function(JsonRow) valueFunc) => Map.fromEntries(table.where((e) => e[keyName] != null).map((e) => getMapEntry(e[keyName] as T, keyFunc, valueFunc)));
+  Map<T1, T2> getMap<T1, T2>(T1 Function(JsonRow) keyFunc, T2 Function(JsonRow) valueFunc) => Map.fromEntries(_table.where((e) => e[keyName] != null).map((e) => getMapEntry(e[keyName] as T, keyFunc, valueFunc)));
 
   /// Add rows to this JsonTable. Only rows with valid Ids will be added. Return a list of IDs
-  List<T> addAll(JsonTable rows, [bool override = false]) => [for (var newRow in rows) add(newRow, override) as T];
+  Iterable<T> addAll(JsonTable rows, [bool override = false]) => [for (var newRow in rows) add(newRow, override)].whereNotNull();
 
   /// Add a row to this JsonTable. Only rows with valid Ids will be added. Return the ID
   T? add(JsonRow row, [bool override = false]) {
@@ -89,8 +88,11 @@ class KeyedJsonTable<T extends Comparable> extends Iterable<JsonRow> {
     if (pk.isValid()) {
       var exist = this[pk];
       if (exist == null || override) {
-        this[pk] = row;
-        return pk;
+        exist ??= row;
+        exist.addAll(row);
+        if (_table.contains(exist) == false) {
+          _table.add(exist);
+        }
       }
     }
     return null;
@@ -98,9 +100,9 @@ class KeyedJsonTable<T extends Comparable> extends Iterable<JsonRow> {
 
   /// Reload the table with the provided rows.
   /// Only rows with valid Ids will be added.
-  void reloadWith(JsonTable rows) {
-    clear();
-    addAll(rows);
+  Iterable<T> reloadWith(JsonTable rows) {
+    _table.clear();
+    return addAll(rows);
   }
 
   /// Returns the value associated with the specified [key] in the [JsonRow].
@@ -109,14 +111,14 @@ class KeyedJsonTable<T extends Comparable> extends Iterable<JsonRow> {
 
   /// Checks if the list contains the specified [key].
   /// Returns `true` if the [key] is found, `false` otherwise.
-  bool containsKey(T key) => this[key] != null;
+  bool containsKey(T key) => _table.map((e) => getKey(e) == key).isNotEmpty;
 
   /// Removes the [JsonRow] with the specified [key] from the list.
   /// Returns the removed [JsonRow], or `null` if the [key] is not found.
-  JsonRow? remove(T key) => table.detachItems((e) => e[keyName] == key).singleOrNull;
+  JsonRow? remove(T key) => _table.detachItems((e) => e[keyName] == key).singleOrNull;
 
   /// Removes all elements from the list.
-  void clear() => table.clear();
+  void clear() => _table.clear();
 
   /// Returns an iterator over the elements in the list.
   ///
@@ -132,7 +134,7 @@ class KeyedJsonTable<T extends Comparable> extends Iterable<JsonRow> {
   /// }
   /// ```
   @override
-  Iterator<JsonRow> get iterator => table.iterator;
+  Iterator<JsonRow> get iterator => _table.iterator;
 }
 
 extension ListMapExtension<K, V> on JsonTable {
