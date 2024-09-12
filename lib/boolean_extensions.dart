63e08f966210ce5114f4678b9e0dfd8c2d7b6a14
Zonaro <zonaro@outlook.com>
2024-09-12 16:46:12 -0300

    Refatorar extensões booleanas e adicionar método toNullableBool

--- lib/boolean_extensions.dart
+++ lib/boolean_extensions.dart
@@ -1,25 +1,9 @@
 extension BooleanExtensions on bool? {
-  /// Returns [true] if the value is not null and true.
-  /// Otherwise [false] is returned.
-  bool get isTrue => this != null && this == true;
-
-  /// Returns [true] if the value is not null and false.
-  /// Otherwise [false] is returned.
-  bool get isFalse => this != null && this == false;
-
-  /// Returns [true] if the value is null or false.
-  /// Otherwise [false] is returned.
-  bool get isNotTrue => this == null || this == false;
-
-  /// Returns [true] if the value is null and true.
-  /// Otherwise [false] is returned.
-  bool get isNotFalse => this == null || this == true;
-
-  bool? get toNullableBool => this;
-
   /// Returns `1` if the value is `true`
   /// otherwise `0` is returned.
   int get toInt => toNullableInt ?? 0;
+
+  bool? get toNullableBool => this;
 
   int? get toNullableInt => this != null ? (this! ? 1 : 0) : null;
 }
