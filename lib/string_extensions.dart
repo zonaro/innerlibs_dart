d741fdd0aa5d18a1b7a7247581a2f0f64e0b4826
Zonaro <zonaro@outlook.com>
2024-10-24 16:20:50 -0300

    Adicionar validação para novos formatos de entrada de dados

--- lib/string_extensions.dart
+++ lib/string_extensions.dart
@@ -21,34 +21,6 @@ extension NullStringExtension on String? {
   /// Returns null if string is blank. Otherwise, returns the string.
   String? get nullIfBlank => ifBlank(null);
 
-  /// Returns the string if it is blank, otherwise returns "0".
-  String get zeroIfBlank => ifBlank("0")!;
-
-  String blankCoalesce(Iterable<string?> newString) {
-    var x = [blankIfNull, ...newString];
-    return x.firstWhereOrNull((e) => e.isNotBlank).blankIfNull;
-  }
-
-  /// Format a string with ICU syntax.
-  ///
-  /// See the [ICU User Guide](https://api.flutter.dev/flutter/message_format/MessageFormat-class.html) for more information.
-  ///
-  /// The [args] parameter is a map of arguments to be used in the string.
-  ///
-  /// The [locale] parameter is the locale to be used in the formatting.  If not specified, the default locale is used.
-  ///
-  /// Returns the formatted string.
-  String format(Map<string, Object> args, [String? locale]) {
-    if (isBlank) {
-      return blankIfNull;
-    }
-    final messageFormat = MessageFormat(this!, locale: locale ?? "en");
-    return messageFormat.format(args);
-  }
-
-  /// Returns the string if it is not null, empty or blank, otherwise returns the specified string.
-  String? ifBlank(String? newString) => this != null && this!.isNotBlank ? this : newString;
-
   /// Splits a string by whitespace, but only those whitespaces that are outside of quotes.
   ///
   /// This function handles both single and double quotes. It splits the input string
@@ -91,17 +63,41 @@ extension NullStringExtension on String? {
     return result;
   }
 
+  /// Returns the string if it is blank, otherwise returns "0".
+  String get zeroIfBlank => ifBlank("0")!;
+
+  String blankCoalesce(Iterable<string?> newString) {
+    var x = [blankIfNull, ...newString];
+    return x.firstWhereOrNull((e) => e.isNotBlank).blankIfNull;
+  }
+
+  /// Format a string with ICU syntax.
+  ///
+  /// See the [ICU User Guide](https://api.flutter.dev/flutter/message_format/MessageFormat-class.html) for more information.
+  ///
+  /// The [args] parameter is a map of arguments to be used in the string.
+  ///
+  /// The [locale] parameter is the locale to be used in the formatting.  If not specified, the default locale is used.
+  ///
+  /// Returns the formatted string.
+  String format(Map<string, Object> args, [String? locale]) {
+    if (isBlank) {
+      return blankIfNull;
+    }
+    final messageFormat = MessageFormat(this!, locale: locale ?? "en");
+    return messageFormat.format(args);
+  }
+
+  /// Returns the string if it is not null, empty or blank, otherwise returns the specified string.
+  String? ifBlank(String? newString) => this != null && this!.isNotBlank ? this : newString;
+
   /// Return left string if not blank. Otherwise return right string.
   String operator |(Object? s) => ifBlank("$s") ?? "";
 }
 
 extension StringExtensions on String {
   static final _defaultDiacriticsRemovalap = [
-    {
-      'base': 'A',
-      'letters':
-          '\u0041\u24B6\uFF21\u00C0\u00C1\u00C2\u1EA6\u1EA4\u1EAA\u1EA8\u00C3\u0100\u0102\u1EB0\u1EAE\u1EB4\u1EB2\u0226\u01E0\u00C4\u01DE\u1EA2\u00C5\u01FA\u01CD\u0200\u0202\u1EA0\u1EAC\u1EB6\u1E00\u0104\u023A\u2C6F'
-    },
+    {'base': 'A', 'letters': '\u0041\u24B6\uFF21\u00C0\u00C1\u00C2\u1EA6\u1EA4\u1EAA\u1EA8\u00C3\u0100\u0102\u1EB0\u1EAE\u1EB4\u1EB2\u0226\u01E0\u00C4\u01DE\u1EA2\u00C5\u01FA\u01CD\u0200\u0202\u1EA0\u1EAC\u1EB6\u1E00\u0104\u023A\u2C6F'},
     {'base': 'AA', 'letters': '\uA732'},
     {'base': 'AE', 'letters': '\u00C6\u01FC\u01E2'},
     {'base': 'AO', 'letters': '\uA734'},
@@ -113,10 +109,7 @@ extension StringExtensions on String {
     {'base': 'D', 'letters': '\u0044\u24B9\uFF24\u1E0A\u010E\u1E0C\u1E10\u1E12\u1E0E\u0110\u018B\u018A\u0189\uA779'},
     {'base': 'DZ', 'letters': '\u01F1\u01C4'},
     {'base': 'Dz', 'letters': '\u01F2\u01C5'},
-    {
-      'base': 'E',
-      'letters': '\u0045\u24BA\uFF25\u00C8\u00C9\u00CA\u1EC0\u1EBE\u1EC4\u1EC2\u1EBC\u0112\u1E14\u1E16\u0114\u0116\u00CB\u1EBA\u011A\u0204\u0206\u1EB8\u1EC6\u0228\u1E1C\u0118\u1E18\u1E1A\u0190\u018E'
-    },
+    {'base': 'E', 'letters': '\u0045\u24BA\uFF25\u00C8\u00C9\u00CA\u1EC0\u1EBE\u1EC4\u1EC2\u1EBC\u0112\u1E14\u1E16\u0114\u0116\u00CB\u1EBA\u011A\u0204\u0206\u1EB8\u1EC6\u0228\u1E1C\u0118\u1E18\u1E1A\u0190\u018E'},
     {'base': 'F', 'letters': '\u0046\u24BB\uFF26\u1E1E\u0191\uA77B'},
     {'base': 'G', 'letters': '\u0047\u24BC\uFF27\u01F4\u011C\u1E20\u011E\u0120\u01E6\u0122\u01E4\u0193\uA7A0\uA77D\uA77E'},
     {'base': 'H', 'letters': '\u0048\u24BD\uFF28\u0124\u1E22\u1E26\u021E\u1E24\u1E28\u1E2A\u0126\u2C67\u2C75\uA78D'},
@@ -130,11 +123,7 @@ extension StringExtensions on String {
     {'base': 'N', 'letters': '\u004E\u24C3\uFF2E\u01F8\u0143\u00D1\u1E44\u0147\u1E46\u0145\u1E4A\u1E48\u0220\u019D\uA790\uA7A4'},
     {'base': 'NJ', 'letters': '\u01CA'},
     {'base': 'Nj', 'letters': '\u01CB'},
-    {
-      'base': 'O',
-      'letters':
-          '\u004F\u24C4\uFF2F\u00D2\u00D3\u00D4\u1ED2\u1ED0\u1ED6\u1ED4\u00D5\u1E4C\u022C\u1E4E\u014C\u1E50\u1E52\u014E\u022E\u0230\u00D6\u022A\u1ECE\u0150\u01D1\u020C\u020E\u01A0\u1EDC\u1EDA\u1EE0\u1EDE\u1EE2\u1ECC\u1ED8\u01EA\u01EC\u00D8\u01FE\u0186\u019F\uA74A\uA74C'
-    },
+    {'base': 'O', 'letters': '\u004F\u24C4\uFF2F\u00D2\u00D3\u00D4\u1ED2\u1ED0\u1ED6\u1ED4\u00D5\u1E4C\u022C\u1E4E\u014C\u1E50\u1E52\u014E\u022E\u0230\u00D6\u022A\u1ECE\u0150\u01D1\u020C\u020E\u01A0\u1EDC\u1EDA\u1EE0\u1EDE\u1EE2\u1ECC\u1ED8\u01EA\u01EC\u00D8\u01FE\u0186\u019F\uA74A\uA74C'},
     {'base': 'OI', 'letters': '\u01A2'},
     {'base': 'OO', 'letters': '\uA74E'},
     {'base': 'OU', 'letters': '\u0222'},
@@ -146,22 +135,14 @@ extension StringExtensions on String {
     {'base': 'S', 'letters': '\u0053\u24C8\uFF33\u1E9E\u015A\u1E64\u015C\u1E60\u0160\u1E66\u1E62\u1E68\u0218\u015E\u2C7E\uA7A8\uA784'},
     {'base': 'T', 'letters': '\u0054\u24C9\uFF34\u1E6A\u0164\u1E6C\u021A\u0162\u1E70\u1E6E\u0166\u01AC\u01AE\u023E\uA786'},
     {'base': 'TZ', 'letters': '\uA728'},
-    {
-      'base': 'U',
-      'letters':
-          '\u0055\u24CA\uFF35\u00D9\u00DA\u00DB\u0168\u1E78\u016A\u1E7A\u016C\u00DC\u01DB\u01D7\u01D5\u01D9\u1EE6\u016E\u0170\u01D3\u0214\u0216\u01AF\u1EEA\u1EE8\u1EEE\u1EEC\u1EF0\u1EE4\u1E72\u0172\u1E76\u1E74\u0244'
-    },
+    {'base': 'U', 'letters': '\u0055\u24CA\uFF35\u00D9\u00DA\u00DB\u0168\u1E78\u016A\u1E7A\u016C\u00DC\u01DB\u01D7\u01D5\u01D9\u1EE6\u016E\u0170\u01D3\u0214\u0216\u01AF\u1EEA\u1EE8\u1EEE\u1EEC\u1EF0\u1EE4\u1E72\u0172\u1E76\u1E74\u0244'},
     {'base': 'V', 'letters': '\u0056\u24CB\uFF36\u1E7C\u1E7E\u01B2\uA75E\u0245'},
     {'base': 'VY', 'letters': '\uA760'},
     {'base': 'W', 'letters': '\u0057\u24CC\uFF37\u1E80\u1E82\u0174\u1E86\u1E84\u1E88\u2C72'},
     {'base': 'X', 'letters': '\u0058\u24CD\uFF38\u1E8A\u1E8C'},
     {'base': 'Y', 'letters': '\u0059\u24CE\uFF39\u1EF2\u00DD\u0176\u1EF8\u0232\u1E8E\u0178\u1EF6\u1EF4\u01B3\u024E\u1EFE'},
     {'base': 'Z', 'letters': '\u005A\u24CF\uFF3A\u0179\u1E90\u017B\u017D\u1E92\u1E94\u01B5\u0224\u2C7F\u2C6B\uA762'},
-    {
-      'base': 'a',
-      'letters':
-          '\u0061\u24D0\uFF41\u1E9A\u00E0\u00E1\u00E2\u1EA7\u1EA5\u1EAB\u1EA9\u00E3\u0101\u0103\u1EB1\u1EAF\u1EB5\u1EB3\u0227\u01E1\u00E4\u01DF\u1EA3\u00E5\u01FB\u01CE\u0201\u0203\u1EA1\u1EAD\u1EB7\u1E01\u0105\u2C65\u0250'
-    },
+    {'base': 'a', 'letters': '\u0061\u24D0\uFF41\u1E9A\u00E0\u00E1\u00E2\u1EA7\u1EA5\u1EAB\u1EA9\u00E3\u0101\u0103\u1EB1\u1EAF\u1EB5\u1EB3\u0227\u01E1\u00E4\u01DF\u1EA3\u00E5\u01FB\u01CE\u0201\u0203\u1EA1\u1EAD\u1EB7\u1E01\u0105\u2C65\u0250'},
     {'base': 'aa', 'letters': '\uA733'},
     {'base': 'ae', 'letters': '\u00E6\u01FD\u01E3'},
     {'base': 'ao', 'letters': '\uA735'},
@@ -172,11 +153,7 @@ extension StringExtensions on String {
     {'base': 'c', 'letters': '\u0063\u24D2\uFF43\u0107\u0109\u010B\u010D\u00E7\u1E09\u0188\u023C\uA73F\u2184'},
     {'base': 'd', 'letters': '\u0064\u24D3\uFF44\u1E0B\u010F\u1E0D\u1E11\u1E13\u1E0F\u0111\u018C\u0256\u0257\uA77A'},
     {'base': 'dz', 'letters': '\u01F3\u01C6'},
-    {
-      'base': 'e',
-      'letters':
-          '\u0065\u24D4\uFF45\u00E8\u00E9\u00EA\u1EC1\u1EBF\u1EC5\u1EC3\u1EBD\u0113\u1E15\u1E17\u0115\u0117\u00EB\u1EBB\u011B\u0205\u0207\u1EB9\u1EC7\u0229\u1E1D\u0119\u1E19\u1E1B\u0247\u025B\u01DD'
-    },
+    {'base': 'e', 'letters': '\u0065\u24D4\uFF45\u00E8\u00E9\u00EA\u1EC1\u1EBF\u1EC5\u1EC3\u1EBD\u0113\u1E15\u1E17\u0115\u0117\u00EB\u1EBB\u011B\u0205\u0207\u1EB9\u1EC7\u0229\u1E1D\u0119\u1E19\u1E1B\u0247\u025B\u01DD'},
     {'base': 'f', 'letters': '\u0066\u24D5\uFF46\u1E1F\u0192\uA77C'},
     {'base': 'g', 'letters': '\u0067\u24D6\uFF47\u01F5\u011D\u1E21\u011F\u0121\u01E7\u0123\u01E5\u0260\uA7A1\u1D79\uA77F'},
     {'base': 'h', 'letters': '\u0068\u24D7\uFF48\u0125\u1E23\u1E27\u021F\u1E25\u1E29\u1E2B\u1E96\u0127\u2C68\u2C76\u0265'},
@@ -189,11 +166,7 @@ extension StringExtensions on String {
     {'base': 'm', 'letters': '\u006D\u24DC\uFF4D\u1E3F\u1E41\u1E43\u0271\u026F'},
     {'base': 'n', 'letters': '\u006E\u24DD\uFF4E\u01F9\u0144\u00F1\u1E45\u0148\u1E47\u0146\u1E4B\u1E49\u019E\u0272\u0149\uA791\uA7A5'},
     {'base': 'nj', 'letters': '\u01CC'},
-    {
-      'base': 'o',
-      'letters':
-          '\u006F\u24DE\uFF4F\u00F2\u00F3\u00F4\u1ED3\u1ED1\u1ED7\u1ED5\u00F5\u1E4D\u022D\u1E4F\u014D\u1E51\u1E53\u014F\u022F\u0231\u00F6\u022B\u1ECF\u0151\u01D2\u020D\u020F\u01A1\u1EDD\u1EDB\u1EE1\u1EDF\u1EE3\u1ECD\u1ED9\u01EB\u01ED\u00F8\u01FF\u0254\uA74B\uA74D\u0275'
-    },
+    {'base': 'o', 'letters': '\u006F\u24DE\uFF4F\u00F2\u00F3\u00F4\u1ED3\u1ED1\u1ED7\u1ED5\u00F5\u1E4D\u022D\u1E4F\u014D\u1E51\u1E53\u014F\u022F\u0231\u00F6\u022B\u1ECF\u0151\u01D2\u020D\u020F\u01A1\u1EDD\u1EDB\u1EE1\u1EDF\u1EE3\u1ECD\u1ED9\u01EB\u01ED\u00F8\u01FF\u0254\uA74B\uA74D\u0275'},
     {'base': 'oi', 'letters': '\u01A3'},
     {'base': 'ou', 'letters': '\u0223'},
     {'base': 'oo', 'letters': '\uA74F'},
@@ -203,11 +176,7 @@ extension StringExtensions on String {
     {'base': 's', 'letters': '\u0073\u24E2\uFF53\u00DF\u015B\u1E65\u015D\u1E61\u0161\u1E67\u1E63\u1E69\u0219\u015F\u023F\uA7A9\uA785\u1E9B'},
     {'base': 't', 'letters': '\u0074\u24E3\uFF54\u1E6B\u1E97\u0165\u1E6D\u021B\u0163\u1E71\u1E6F\u0167\u01AD\u0288\u2C66\uA787'},
     {'base': 'tz', 'letters': '\uA729'},
-    {
-      'base': 'u',
-      'letters':
-          '\u0075\u24E4\uFF55\u00F9\u00FA\u00FB\u0169\u1E79\u016B\u1E7B\u016D\u00FC\u01DC\u01D8\u01D6\u01DA\u1EE7\u016F\u0171\u01D4\u0215\u0217\u01B0\u1EEB\u1EE9\u1EEF\u1EED\u1EF1\u1EE5\u1E73\u0173\u1E77\u1E75\u0289'
-    },
+    {'base': 'u', 'letters': '\u0075\u24E4\uFF55\u00F9\u00FA\u00FB\u0169\u1E79\u016B\u1E7B\u016D\u00FC\u01DC\u01D8\u01D6\u01DA\u1EE7\u016F\u0171\u01D4\u0215\u0217\u01B0\u1EEB\u1EE9\u1EEF\u1EED\u1EF1\u1EE5\u1E73\u0173\u1E77\u1E75\u0289'},
     {'base': 'v', 'letters': '\u0076\u24E5\uFF56\u1E7D\u1E7F\u028B\uA75F\u028C'},
     {'base': 'vy', 'letters': '\uA761'},
     {'base': 'w', 'letters': '\u0077\u24E6\uFF57\u1E81\u1E83\u0175\u1E87\u1E85\u1E98\u1E89\u2C73'},
@@ -762,8 +731,7 @@ extension StringExtensions on String {
     if (isBlank) {
       return false;
     }
-    var regex = RegExp(
-        r'^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$');
+    var regex = RegExp(r'^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$');
     if (regex.hasMatch(this)) {
       return true;
     }
@@ -1021,8 +989,7 @@ extension StringExtensions on String {
       return false;
     }
     // uses regex for url (with or without port) or IP (with or withou port)
-    return RegExp(r'^((http|https):\/\/)?(www\.)?([a-zA-Z0-9]+([a-zA-Z0-9-]+[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}(:[0-9]{1,5})?$|^((http|https):\/\/)?([0-9]{1,3}\.){3}[0-9]{1,3}(:[0-9]{1,5})?$')
-        .hasMatch(this);
+    return RegExp(r'^((http|https):\/\/)?(www\.)?([a-zA-Z0-9]+([a-zA-Z0-9-]+[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}(:[0-9]{1,5})?$|^((http|https):\/\/)?([0-9]{1,3}\.){3}[0-9]{1,3}(:[0-9]{1,5})?$').hasMatch(this);
   }
 
   /// Checks if the string is a valid EAN (European Article Number) barcode.
@@ -1104,7 +1071,7 @@ extension StringExtensions on String {
 
   /// Returns the integer representation of the string, considering only the numeric characters.
   /// If the string does not contain any numeric characters, it returns null.
-  int? get onlyNumbersInt => onlyNumbers.toInt;
+  int get onlyNumbersInt => onlyNumbers.toInt ?? 0;
 
   /// Splits a pascal case string into individual words.
   /// Returns a list of strings representing the words in the pascal case string.
@@ -2537,8 +2504,7 @@ extension StringExtensions on String {
   /// - `?` matches any single character.
   ///
   /// Returns `true` if the [value] matches the [mask] pattern, `false` otherwise.
-  bool isLike(string mask, [bool caseSensitive = false]) =>
-      RegExp('^${RegExp.escape(mask).replaceAll('\\*', '.*').replaceAll('\\?', '.').toString()}\$', multiLine: true, caseSensitive: caseSensitive).hasMatch(this);
+  bool isLike(string mask, [bool caseSensitive = false]) => RegExp('^${RegExp.escape(mask).replaceAll('\\*', '.*').replaceAll('\\?', '.').toString()}\$', multiLine: true, caseSensitive: caseSensitive).hasMatch(this);
 
   bool isNotIn(Iterable<String> strings) => !isIn(strings);
 
@@ -2698,8 +2664,7 @@ extension StringExtensions on String {
   /// String foo =  'Hello dear friend how you doing ?';
   /// int readTime = foo.readTime(); // returns 3 seconds.
   /// ```
-  Duration readTime({int wordsPerDuration = 200, Duration duration = const Duration(minutes: 1)}) =>
-      isBlank || wordsPerDuration == 0 ? 0.seconds : (countWords / (wordsPerDuration * duration.inSeconds)).seconds;
+  Duration readTime({int wordsPerDuration = 200, Duration duration = const Duration(minutes: 1)}) => isBlank || wordsPerDuration == 0 ? 0.seconds : (countWords / (wordsPerDuration * duration.inSeconds)).seconds;
 
   /// Removes everything in the `String` after the first match of the [pattern].
   ///
@@ -3030,7 +2995,7 @@ extension StringExtensions on String {
   /// The placeholders are indexes surrounded by [openWrapChar] and [closeWrapChar].
   String replaceWrappedWithList({required Iterable<dynamic> values, required String openWrapChar, String? closeWrapChar}) {
     if (isBlank) return blankIfNull;
-    return replaceWrappedWithMap(values: Map.fromEntries(values.select((x,i) => MapEntry(i.toString(), x))), openWrapChar: openWrapChar, closeWrapChar: closeWrapChar);
+    return replaceWrappedWithMap(values: Map.fromEntries(values.select((x, i) => MapEntry(i.toString(), x))), openWrapChar: openWrapChar, closeWrapChar: closeWrapChar);
   }
 
   /// Replaces the placeholders in the `String` with the values from the provided [values] map.
